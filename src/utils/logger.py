03c20fd6c1c53add1625e96c4be6745a8fe07733
DDataAly <alyona.dorof@gmail.com>
2025-07-30 12:04:45 +0100

    Adds logger to monitor program execution

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,14 +1,13 @@
 import logging
 import os
 
+
+# Can't be changer once the code has run
 def setup_logging():
-    os.makedirs('data', exist_ok=True)
+    os.makedirs("data", exist_ok=True)
     logging.basicConfig(
-        filename='data/log.txt',
+        filename="data/log.txt",
         level=logging.INFO,
-        format='%(asctime)s [%(levelname)s] %(message)s',
-        filemode='w'
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        filemode="w",
     )
-
-
-
