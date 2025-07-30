af5144fe30e035789bda26f75805291818d31985
DDataAly <alyona.dorof@gmail.com>
2025-07-30 12:09:23 +0100

    Adds type conversion at rounding stage

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -10,7 +10,7 @@ def get_request_time() -> str:
 
 def create_best_price_df(best_price: list) -> pd.DataFrame:
     best_price_dict = {
-        "price": round(best_price[0], 2),
+        "price": round(float(best_price[0]), 2),
         "exchange": best_price[1],
         "request_time": best_price[2],
     }
@@ -21,10 +21,4 @@ def create_best_price_df(best_price: list) -> pd.DataFrame:
 def record_best_price(best_price_df: pd.DataFrame, path: str):
     file_name = f"{path}/best_deal.csv"
     col_headers = False if os.path.isfile(file_name) else True
-    best_price_df.to_csv(
-        file_name,
-        mode="a",
-        header=col_headers,
-        index=False
-    )
-    print("Fetching new price...")
+    best_price_df.to_csv(file_name, mode="a", header=col_headers, index=False)
