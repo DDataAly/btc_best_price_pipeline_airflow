97474e10b286f4c5db8a9078c356668e0b5a2746
DDataAly <alyona.dorof@gmail.com>
2025-09-02 12:49:31 +0100

    Add run_best_price_fetch task

--- dag/btc_price_dag.py
+++ dag/btc_price_dag.py
@@ -1,7 +1,13 @@
+import os
 from airflow.sdk import DAG
 from airflow.decorators import task
 from airflow.models import Variable
 from datetime import datetime
+from exchanges.binance import BinanceExchange
+from exchanges.kraken import KrakenExchange
+from exchanges.coinbase import CoinbaseExchange
+from utils.get_best_price import find_best_price
+from utils.helpers import create_best_price_df, record_best_price
 
 default_args = {
     "owner": "DDataAly",
@@ -18,7 +24,7 @@ with DAG(
 ) as dag:
 
     @task
-    def init_parameters():
+    def init_order_parameters():
         """
         Since it's a pipeline there is no user input => we need to initialise order parameters first.
         Uses Airflow Variables with defaults if not set.
@@ -36,8 +42,46 @@ with DAG(
             "vol_per_order": vol_per_order,
         }
 
+    @task
+    def set_up_exchanges():
+        kraken = KrakenExchange(
+            "kraken",
+            "https://api.kraken.com/0/public/Depth",
+            {"pair": "XXBTZUSD", "count": 20},
+            "XXBTZUSD",
+        )
+        binance = BinanceExchange(
+            "binance",
+            "https://api.binance.com/api/v3/depth",
+            {"symbol": "BTCUSDT", "limit": 20},
+            "BTCUSDT",
+        )
+        coinbase = CoinbaseExchange(
+            "coinbase",
+            "https://api.exchange.coinbase.com/products/BTC-USD/book",
+            {"level": 2},
+            "BTC-USD",
+        )
+        return [kraken, binance, coinbase]
 
-    params = init_parameters()
+    @task
+    def run_best_price_fetch(params : dict, exchanges : list) -> float:
+        total_cost = []
+
+        base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        path = os.path.join(base_dir, "data")
+        os.makedirs(path, exist_ok=True)
+
+        for i in range(params["num_trans"]):
+            best_price = find_best_price(exchanges, path, params["vol_per_order"])
+            best_price_df = create_best_price_df(best_price)
+            record_best_price(best_price_df, path)
+            total_cost.append(best_price[0])
+        
+        return sum(total_cost)
 
 
+    params = init_order_parameters()
+    exchanges = set_up_exchanges()
+    total_cost = run_best_price_fetch(params, exchanges)
 
